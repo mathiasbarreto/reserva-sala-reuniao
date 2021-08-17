4016ba3ba67cdfc9996ac7e0e669301a06fac5a1
mathiasbarreto <mathiasbarreto.dev@gmail.com>
2021-08-16 23:15:36 -0300

    booking model testing implemented

--- test/models/booking_test.rb
+++ test/models/booking_test.rb
@@ -2,6 +2,20 @@ require 'test_helper'
 
 class BookingTest < ActiveSupport::TestCase
   # test "the truth" do
-  #   assert true
+  # assert true
   # end
+  
+  test 'booking is valid with valid attributes' do
+    today = Date.today
+    user = users(:fulano)
+    booking = Booking.new(
+     user: user, 
+      day: today.day,
+      hour: Time.now.hour,
+      week: today.cweek,
+      month: today.month,
+      year: today.year
+    )
+    assert booking.valid?
+  end
 end
